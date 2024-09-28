9e0f06536e77c379c5de126191d4e09d174dc6ff
Robi1403 <rodejojohnrobert@gmail.com>
2024-09-28 15:40:08 +0800

    comment added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
 
         <main class="py-4 min-vh-100">
             @yield('content')
+            {{-- COMMENT WILL SHOW HERE --}}
         </main>
     @include('components.footer')
     </div>
