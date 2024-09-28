d3cecf94a9ca2e0f288b2ee71e8545aac6dc0044
Robi1403 <rodejojohnrobert@gmail.com>
2024-09-28 22:08:46 +0800

    added sidenav for bussiness manager side

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="{{ asset('images/logo.png') }}">
 
-        <title>{{ config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('images/logo.png') }}">
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-    </head>
-    <body class="antialiased">
-        @include('components.navbar')
+    <title>{{ config('app.name') }}</title>
 
-        {{-- delete if landing page is ready --}}
-        @include('styleguide')
-    </body>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+</head>
+
+<body class="antialiased">
+    {{-- @include('components.navbar')
+
+    {{-- delete if landing page is ready
+    @include('styleguide') --}}
+
+    <div>
+        @include('components.sidenavBuss');
+    </div>
+</body>
+
 </html>
