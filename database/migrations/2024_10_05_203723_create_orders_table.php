15944b484e2ef59d62a8d92ae9affc8e4e36cf04
Vicente Bercasio <vicentetobio00@gmail.com>
2024-10-06 05:09:14 +0800

    Created and Reviewed all db tables with models and controller files

---
+++ database/migrations/2024_10_05_203723_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('shop_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('order_status_id'); 
+
+            $table->float('total_amount'); 
+            $table->float('supplier_price_total_amount'); 
+            $table->integer('total_items');
+            $table->timestamp('order_date'); 
+            $table->integer('reference_number'); 
+
+            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('order_status_id')->references('id')->on('statuses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
