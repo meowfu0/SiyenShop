b801a6cfee91afe1a4a106e63f1d91ffd2bace2d
Archie Onoya <archieonoya13@gmail.com>
2024-10-06 22:50:56 +0800

    update admin-sidenav font size

--- resources/views/livewire/admin/admin-sidenav.blade.php
+++ resources/views/livewire/admin/admin-sidenav.blade.php
@@ -4,38 +4,38 @@
         <img class="logo-img" src="{{ asset('images/logo.png') }}" alt="">
     </a>
 
-    <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'admin.dashboard' ? 'active' : '' }}" 
+    <a class="sidenav-link ps-4 d-flex gap-4 fs-4 fw-medium {{ Route::currentRouteName() == 'admin.dashboard' ? 'active' : '' }}" 
        href="{{ route('admin.dashboard') }}" wire:navigate>
         <img src="{{ asset('images/dashboard.svg') }}" alt="">
         Dashboard
     </a>
 
-    <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'admin.users' ? 'active' : '' }}" 
+    <a class="sidenav-link ps-4 d-flex gap-4 fs-4 fw-medium {{ Route::currentRouteName() == 'admin.users' ? 'active' : '' }}" 
        href="{{ route('admin.users') }}" wire:navigate>
         <img src="{{ asset('images/users.svg') }}" alt="">
         Users
     </a>
 
-    <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'admin.shops' ? 'active' : '' }}" 
+    <a class="sidenav-link ps-4 d-flex gap-4 fs-4 fw-medium {{ Route::currentRouteName() == 'admin.shops' ? 'active' : '' }}" 
        href="{{ route('admin.shops') }}" wire:navigate>
         <img src="{{ asset('images/shops.svg') }}" alt="">
         Shops
     </a>
 
-    <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'admin.faqs' ? 'active' : '' }}" 
+    <a class="sidenav-link ps-4 d-flex gap-4 fs-4 fw-medium {{ Route::currentRouteName() == 'admin.faqs' ? 'active' : '' }}" 
        href="{{ route('admin.faqs') }}" wire:navigate>
         <img src="{{ asset('images/faqs.svg') }}" alt="">
         FAQs
     </a>
 
-    <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'admin.chat' ? 'active' : '' }}" 
+    <a class="sidenav-link ps-4 d-flex gap-4 fs-4 fw-medium {{ Route::currentRouteName() == 'admin.chat' ? 'active' : '' }}" 
        href="{{ route('admin.chat') }}" wire:navigate>
         <img src="{{ asset('images/chat.svg') }}" alt="">
         Chat
     </a>
 
     <div class="mt-auto">
-        <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'shop.logout' ? 'active' : '' }}" 
+        <a class="sidenav-link ps-4 d-flex gap-4 fs-4 fw-medium {{ Route::currentRouteName() == 'shop.logout' ? 'active' : '' }}" 
            href="" wire:navigate>
             <img src="{{ asset('images/logout.svg') }}" alt="">
             Logout
