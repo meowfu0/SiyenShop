fcc164b9867eefd9bdbe1b466ee054150128be51
Yaji <yajii3835@gmail.com>
2024-10-06 20:55:38 +0800

    Frontend of the chat for the business manager side

---
+++ resources/views/busmngr.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/custom.js') }}" defer></script>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            display: flex;
+            margin: 0;
+            padding: 0;
+        }
+       
+        .content-chatbox-chat {
+            flex-grow: 1;
+            display: flex;
+            
+        }
+        .content header h1 {
+            font-size: 24px;
+            font-weight: bold;
+        }
+        .container-chatbox {
+            width: 24%;
+            height: 100%;
+            display: flex;          
+            align-items: center;  
+            border-right: 1px solid #ccc;
+            flex-direction: column;
+        }
+        .container-chatbox input {
+            width: 90%;    
+            margin-bottom: 20px;          
+        }
+        .container-btn{
+            display: flex;
+            width: 90%;  
+            height: 48px; 
+            margin-bottom: 0.5rem;   
+            font-weight: bold;
+            font-size: 12px;
+        }
+        .container-chat {
+            width: 76%;
+        }
+        
+        @media (max-width: 768px) {
+            
+            .container-chatbox {
+                width: 30%;
+            }
+        }
+        @media (max-width: 420px) {
+            
+            .container-chatbox {
+                width: 100%;
+            }
+            .container-chat {
+                display: none
+            };
+        }
+        
+       
+    </style>
+
+</head>
+<body>
+    <div class="sidenav">
+        @include('components.busmngr_sidebar') <!-- Sidebar content -->
+    </div>
+    
+</body>
+
+
+</html>
---
+++ resources/views/customer_support/busmngr_chat.blade.php
@@ -0,0 +1,58 @@
+@include('busmngr')
+
+<div class="d-flex flex-grow-1 flex-column">
+    <header class="p-4 border-bottom d-flex align-items-center mt-3">
+        <img src="{{asset('images/chat.svg')}}" alt="" class="me-2" style="height: 23px; width: 23px;">
+        <h1 class="mt-0 text-primary fs-24 fw-bold">Chat</h1>
+    </header>
+
+    <div class="content-chatbox-chat">
+    <!-- Tabs (buttons) -->
+    <div class="container-chatbox">
+        <input type="text" placeholder="Search..." class="form-control mt-3">
+
+
+        <div class="container-btn border d-flex align-items-center" onclick="showChat('jay')" style="cursor: pointer; border-radius: 8px;">
+            <img src="{{asset('images/user.svg')}}" alt="" style="margin-left: 10px;">
+            <span class="text-primary text-center ms-2">Jay Bombales</span>
+        </div>
+
+        
+        <div class="container-btn border d-flex align-items-center" onclick="showChat('john')" style="cursor: pointer; border-radius: 8px;">
+            <img src="{{asset('images/user.svg')}}" alt="" style="margin-left: 10px;">
+            <span class="text-primary text-center ms-2">John Dave Banas</span>
+        </div>
+    </div>
+
+    <div class="container-chat" id="jay" style="display: none; position: relative;">
+        
+        <div class="chat-input d-flex p-3 border-top" style="position: absolute; bottom: 0; left: 0; right: 0; background-color: white;">
+
+            <button type="button" class="d-flex align-center justify-center border-non bg-w" style="display: flex; align-items: center; justify-content: center; outline: none; border: none; background-color: #ffff">
+                <img src="{{asset('images/file.svg')}}" alt="Send" style="width: 20px; height: 20px;">
+            </button>
+            <input type="text" class="form-control me-2" placeholder="Type a message...">
+            <button type="button" class="d-flex align-center justify-center border-non bg-w" style="display: flex; align-items: center; justify-content: center; outline: none; border: none; background-color: #ffff">
+                <img src="{{asset('images/send.svg')}}" alt="Send" style="width: 20px; height: 20px;">
+            </button>
+
+        </div>
+    </div>
+
+    <div class="container-chat" id="john" style="display: none; position: relative;">
+        
+        <div class="chat-input d-flex p-3 border-top" style="position: absolute; bottom: 0; left: 0; right: 0; background-color: white;">
+
+            <button type="button" class="d-flex align-center justify-center border-non bg-w" style="display: flex; align-items: center; justify-content: center; outline: none; border: none; background-color: #ffff">
+                <img src="{{asset('images/file.svg')}}" alt="Send" style="width: 20px; height: 20px;">
+            </button>
+            <input type="text" class="form-control me-2" placeholder="Type a message...">
+            <button type="button" class="d-flex align-center justify-center border-non bg-w" style="display: flex; align-items: center; justify-content: center; outline: none; border: none; background-color: #ffff">
+                <img src="{{asset('images/send.svg')}}" alt="Send" style="width: 20px; height: 20px;">
+            </button>
+
+        </div>
+    </div>
+</div>
+
+</div>
