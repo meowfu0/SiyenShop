49e1c69050bdc153cb73fda3dddf5c513fe58b48
Robi1403 <rodejojohnrobert@gmail.com>
2024-10-07 21:08:22 +0800

    fix sidenav issue

--- resources/views/livewire/shop-sidenav.blade.php
+++ resources/views/livewire/shop-sidenav.blade.php
@@ -1,29 +1,28 @@
-<!-- resources/views/livewire/navigation.blade.php -->
-<nav class="d-flex nav flex-column p-4 border-end min-vh-100 gap-2" style="width: 240px">
-    <a class="nav-link d-flex gap-4 fs-5 fw-medium mb-5 justify-content-center" href="/home" wire:click.prevent="navigateTo('home')">
+<nav class="d-flex nav flex-column p-4 border-end gap-2" style="width: 240px; position: sticky; top: 0; height: 100vh;">
+    <a class="nav-link d-flex gap-4 fs-5 fw-medium mb-5 justify-content-center" href="/home" wire:navigate>
         <img class="logo-img" src="{{ asset('images/logo.png') }}" alt="">
     </a>
 
     <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'shop.dashboard' ? 'active' : '' }}" 
-       href="{{ route('shop.dashboard') }}" wire:click.prevent="navigateTo('shop.dashboard')">
+       href="{{ route('shop.dashboard') }}" wire:navigate>
         <img src="{{ asset('images/dashboard.svg') }}" alt="">
         Dashboard
     </a>
 
     <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'shop.products' ? 'active' : '' }}" 
-       href="{{ route('shop.products') }}" wire:click.prevent="navigateTo('shop.products')">
+       href="{{ route('shop.products') }}" wire:navigate>
         <img src="{{ asset('images/products.svg') }}" alt="">
         Products
     </a>
 
     <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'shop.orders' ? 'active' : '' }}" 
-       href="{{ route('shop.orders') }}" wire:click.prevent="navigateTo('shop.orders')">
+       href="{{ route('shop.orders') }}" wire:navigate>
         <img src="{{ asset('images/orders.svg') }}" alt="">
         Orders
     </a>
 
     <a class="sidenav-link ps-4 d-flex gap-4 fs-5 fw-medium {{ Route::currentRouteName() == 'shop.chat' ? 'active' : '' }}" 
-       href="{{ route('shop.chat') }}" wire:click.prevent="navigateTo('shop.chat')">
+       href="{{ route('shop.chat') }}" wire:navigate>
         <img src="{{ asset('images/chat.svg') }}" alt="">
         Chat
     </a>
