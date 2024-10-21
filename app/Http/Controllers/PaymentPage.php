4f8338e5df57356366a4388b1effc09179595c40
Robi1403 <rodejojohnrobert@gmail.com>
2024-10-21 09:29:32 +0800

    added routes in the nav bar

--- app/Http/Controllers/PaymentPage.php
+++ app/Http/Controllers/PaymentPage.php
@@ -1,13 +1,10 @@
-
-
-
 <?php
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class paymentPage extends Controller
+class PaymentPage extends Controller
 {
     public function index()
     {
