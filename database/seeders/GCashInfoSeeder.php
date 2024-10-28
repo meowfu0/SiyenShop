68c630063f4dbd2d542376eb9d42ef6580fae24b
Robi1403 <rodejojohnrobert@gmail.com>
2024-10-28 22:17:48 +0800

    added some seeders working on backend

---
+++ database/seeders/GCashInfoSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class GCashInfoSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('g_cash_infos')->insert([
+            'user_id' => 1,
+            'shop_id' => 1,
+            'gcash_name' => 'Shop One GCash',
+            'gcash_number' => 9123456789,
+            'gcash_limit' => 5000,
+            'created_at' => Carbon::now(),
+        ]);
+    }
+}
