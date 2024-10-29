77f8cbce35687031de965d2712aed24e4ac23594
Vicente Bercasio <vicentetobio00@gmail.com>
2024-10-29 18:07:54 +0800

    add some seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(CoursesTableSeeder::class);
-
+        $this->call(CartsTableSeeder::class);
     }
 }
