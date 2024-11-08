a1e9d16e4573e1be0a9d1c04e37902e5394de73c
Jervy Jake Morales <jeikudevs@gmail.com>
2024-11-08 20:59:46 +0800

    Fixed some code in CartPage

--- resources/views/user/cartPage.blade.php
+++ resources/views/user/cartPage.blade.php
@@ -1,39 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
+    
     <script src="{{ asset('js/cart.js') }}"></script>
+    
 
     <div class="container pt-5">
-        <div class="row d-flex align-items-center h-100">
-            <div class="col-11">
-                <div class="d-flex justify-content-start align-items-center mb-2">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12  col-md-12 col-lg-12  col-xl-12">
+
+                <div class="d-flex justify-content-start align-items-center mb-2">  
                     <img src="{{ asset('images/cart.svg') }}" class="cart-logo mb-2">
                     <h1 class="ms-1 text-primary fw-bold">Cart</h1>
                 </div>
 
                 <!------- Shirt Sample ------->                
-                <div class="card border border-2 rounded-3 border-gray mb-2 p-2">
-                    <div class="card-body p-2 d-flex justify-content-between">
-                        <div class="d-flex flex-row align-items-center">
-                            <div class="p-4 d-flex align-items-left form-check">
+           
+                    <div class="card-body  d-flex justify-content-between border border-2 rounded-3 border-grey p-2 mb-2"id="main-card" >
+                        <div class="d-flex flex-row align-items-center" >
+                            <div class="p-4 d-flex align-items-left" id="form-check">
                                 <input type="checkbox" class="form-check-input border border-primary checkboxs" 
                                        onclick="AddCheckedProducts()" id="item1" value="350">
                             </div>    
-                            <div class=""> 
+                            <div> 
                                 <img src="{{ asset('images/sample-shirt.jpg') }}" class="img-fluid rounded-2 img-items">
                             </div>
                         </div>
+
+          <div class="d-flex align-items-start flex-row justify-content-between gap-5" id="ProductName">
                         <div class="d-flex align-items-start flex-column">
-                            <div class="p-2"><b>Item</b></div>
-                            <div class="p-2"><span>CIRCUITS T-Shirt - Black</span></div>
+                            <div class="p-2 text-primary" id="remove"><b>Item</b></div>
+                            <div class="p-2 fixed-width" id="prod-name"><p class="mt-2" id="name">CIRCUITS T-Shirt - Black</p></div>
                         </div>
+
+         <div class="d-flex align-items-start flex-row gap-5" id="Price-Variant" > 
                         <div class="d-flex align-items-center flex-column">
-                            <div class="p-2"><b>Unit Price</b></div>
-                            <div class="p-2"><span class="price">₱ 350.00</span></div>
+                            <div class="p-2  text-primary" id="remove" ><b>Unit Price</b></div>
+                            <div class="p-2" id="amount"><p class="price mt-2" >₱ 350.00</p></div>
                         </div>
+                         
                         <div class="d-flex align-items-center flex-column">
-                            <div class="p-2"><b>Variant/Size</b></div>
-                            <div class="p-2">
+                            <div class="p-2  text-primary" id="remove"><b>Variant/Size</b></div>
+                            <div class="p-2  text-primary">
                                 <select class="form-select border border-primary size-input">
                                     <option selected>Large</option>
                                     <option>Medium</option>
@@ -42,103 +50,113 @@
                                 </select>
                             </div>
                         </div>
-                        <div class="d-flex align-items-center flex-column">
-                            <div class="p-2"><b>Quantity</b></div>
-                            <div class="input-group mt-2 border rounded border-primary" style="overflow: hidden; height: 34px;">
-                                <button class="btn" type="button" onclick="decrementQuantity('quantity_item1')">-</button>
+                    </div>
+                 </div>
+                        <div class="d-flex align-items-center flex-column"id="input-quantity">
+                            <div class="p-2  text-primary" id="remove"><b>Quantity</b></div>
+                            <div class="input-group mt-2 border rounded border-primary" id="Quantity-input" style="overflow: hidden; height: 34px;">
+                                <button class="btn no-hover" id="buttons" type="button" onclick="decrementQuantity('quantity_item1')">-</button>
                                 <input id="quantity_item1" min="0" name="quantity" value="1" type="text" readonly
                                        class="form-control text-center outline-primary" style="width: 38px; border: none;">
-                                <button class="btn" type="button" onclick="incrementQuantity('quantity_item1')">+</button>
+                                <button class="btn no-hover" id="buttons" type="button" onclick="incrementQuantity('quantity_item1')">+</button>
                             </div>
                         </div>
-                        <div class="d-flex align-items-center flex-column">
-                            <div class="p-2"><b>Total</b></div>
-                            <div class="p-2">₱ 350.00</div>
+                        <div class="d-flex align-items-center flex-column" id="remove">
+                            <div class="p-2  text-primary"id="remove"><b>Total</b></div>
+                            <div class="p-2"><p class="mt-1">₱ 250.00 </p></div> 
                         </div>
-                        <div class="d-flex flex-row align-items-center">
-                            <div class="p-4 d-flex align-items-left">
-                                <button type="button" class="btn">
-                                    <img src="{{ asset('images/trash3.svg') }}" class="mb-1">
+                        <div class="d-flex flex-row align-items-center" id="trashbin">
+                            <div class="p-4 d-flex align-items-left" id="trashbin">
+                                <button type="button" class="btn" id="trashbin-main">
+                                    <img src="{{ asset('images/trash3.svg') }}" class="mt-1">
                                 </button>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <!--------- Lanyard Sample ------------------->
-                <div class="card rounded-3 border border-2 border-gray mb-4 p-2 ">
-                    <div class="card-body p-2 d-flex justify-content-between">
-                        <div class="d-flex flex-row align-items-center">
-                            <div class="p-4 d-flex align-items-left form-check">
+                                    <!------- Lanyard Sample ------->                
+           
+                    <div class="card-body  d-flex justify-content-between border border-2 rounded-3 border-grey p-2 mb-2"id="main-card" >
+                        <div class="d-flex flex-row align-items-center" >
+                            <div class="p-4 d-flex align-items-left" id="form-check">
                                 <input type="checkbox" class="form-check-input border border-primary checkboxs" 
                                        onclick="AddCheckedProducts()" id="item2" value="250">
-                            </div>
-                            <div>
-                                <img src="{{ asset('images/lanyard-sample.jpg') }}" class="img-fluid rounded-2 img-items">
-                            </div>
-                        </div> 
-                        <div class="d-flex align-items-start flex-column">
-                            <div class="p-2"><b>Item</b></div>
-                            <div class="p-2"><span>CSC Reversible Lanyard</span></div>
+                            </div>    
+                            <div> 
+                            <img src="{{ asset('images/lanyard-sample.jpg') }}" class="img-fluid rounded-2 img-items"></div>
                         </div>
+
+          <div class="d-flex align-items-start flex-row justify-content-between gap-5" id="ProductName">
+                        <div class="d-flex align-items-start flex-column">
+                            <div class="p-2  text-primary" id="remove"><b>Item</b></div>
+                            <div class="p-2  fixed-width" id="prod-name"><p class="mt-2" id="name">CSC Reversible Lanyard
+                            </p></div>
+                        </div>
+
+         <div class="d-flex align-items-start flex-row gap-5" id="Price-Variant" > 
                         <div class="d-flex align-items-center flex-column">
-                            <div class="p-2"><b>Unit Price</b></div>
-                            <div class="p-2">₱ 250.00</div> 
-                        </div> 
+                            <div class="p-2  text-primary" id="remove" ><b>Unit Price</b></div>
+                            <div class="p-2" id="amount"><p class="price mt-2" >₱ 250.00</p></div>
+                        </div>
+                         
                         <div class="d-flex align-items-center flex-column">
-                            <div class="p-2"><b>Variant/Size</b></div>
+                            <div class="p-2  text-primary" id="remove"><b>Variant/Size</b></div>
                             <div class="p-2">
-                                <select class="form-select border border-primary size-input"> 
-                                    <option selected>None</option>
+                                <select class="form-select border border-primary size-input">
+                                    <option selected>none</option>
                                 </select>
                             </div>
                         </div>
-                        <div class="d-flex align-items-center flex-column">
-                            <div class="p-2"><b>Quantity</b></div>
-                            <div class="input-group mt-2 border rounded border-primary" style="overflow: hidden; height: 34px;">
-                                <button class="btn" type="button" onclick="decrementQuantity('quantity_item2')">-</button>
+                    </div>
+                 </div>
+                        <div class="d-flex align-items-center flex-column"id="input-quantity">
+                            <div class="p-2  text-primary" id="remove"><b>Quantity</b></div>
+                            <div class="input-group mt-2 border rounded border-primary" id="Quantity-input" style="overflow: hidden; height: 34px;">
+                                <button class="btn no-hover" id="buttons" type="button" onclick="decrementQuantity('quantity_item2')">-</button>
                                 <input id="quantity_item2" min="0" name="quantity" value="1" type="text" readonly
                                        class="form-control text-center outline-primary" style="width: 38px; border: none;">
-                                <button class="btn" type="button" onclick="incrementQuantity('quantity_item2')">+</button>
+                                <button class="btn no-hover" id="buttons" type="button" onclick="incrementQuantity('quantity_item2')">+</button>
                             </div>
                         </div>
-                        <div class="d-flex align-items-center flex-column">
-                            <div class="p-2"><b>Total</b></div>
-                            <div class="p-2">₱ 250.00</div>
+
+                        <div class="d-flex align-items-center flex-column" id="remove">
+                            <div class="p-2  text-primary"id="remove"><b>Total</b></div>
+                            <div class="p-2"><p class="mt-2">₱ 250.00 </p></div>    
                         </div>
-                        <div class="d-flex flex-row align-items-center">
-                            <div class="p-4 d-flex align-items-left">
-                                <button type="button" class="btn">
-                                    <img src="{{ asset('images/trash3.svg') }}" class="mb-1">
+                        <div class="d-flex flex-row align-items-center" id="trashbin">
+                            <div class="p-4 d-flex align-items-left" id="trashbin">
+                                <button type="button" class="btn" id="trashbin-main">
+                                    <img src="{{ asset('images/trash3.svg') }}" class="mt-2">
                                 </button>
                             </div>
                         </div>
                     </div>
-                </div>
+ 
 
                 <!-- Select All and Checkout Section -->
-                <div class="col-12 fixed-bottom">
-                    <div class="container px-5 py-4 bg-white">
+                <div class="col-12 col-md-12 col-lg-12  col-xl-12 fixed-bottom">
+                    <div class="container px-5 py-3 bg-white" id="fixed-bottom-div1">
                         <div class="col-12 mt-2">
-                            <div class="d-flex align-items-center">
+                        <div class="align-items-center" id="fixed-bottom-div4">
                                 <input type="checkbox" id="selectAll" onclick="toggleSelectAll(this)" class="form-check-input border border-primary checkbox-all">
-                                <h5 class="fs-3 fw-bold mb-0 ms-2 mt-1">Select All (<span id="selectAll-count">2</span>)</h5>
+                                <label class="fs-3 fw-bold mb-0 ms-1">Select All (<span id="selectAll-count" class="fs-2">2</span>)</label>
                             </div>
                         </div>
-                        <hr>
+                        <hr class="hr-fixed">
                         <!-- No. of Items and Total Section -->
-                        <div class="d-flex justify-content-between align-items-center px-5">
+                        <div class="d-flex justify-content-between align-items-center px-4" id="fixed-bottom-div2">
                             <!-- Item count display -->
-                            <div class="col-2 fw-bold">No. of Items: <span id="item-count" class="fw-bold fs-3">0</span> item(s)</div>
+    
+                            <div class="col-2  fs-4 fw-bold" id="items-display">No. of Items: <span id="item-count" class="fw-bold fs-4 text-primary">0</span> item(s)</div>
                             <!-- Total amount display -->
                             <div>
-                                <span class="ms-5 fw-bold fs-4 col-6">TOTAL: ₱ <span id="total-amount" class="fs-4">0</span>.00</span>
+                                <span class="ms-5 fw-bold fs-4 col-6" id="total-info">TOTAL: ₱ <span id="total-amount" class="fs-4 text-primary">0</span>.00</span>
                             </div>
                             <!-- Proceed to Checkout button -->
                             <div>
-                                <a href="#" data-bs-toggle="modal" data-bs-target="#ModalProceed" class="btn btn-primary btn-md p-2 d-flex px-4 gap-2">
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#ModalProceed" class="btn btn-primary btn-md" id="button-size">
                                     Proceed To Checkout
-                                    <img src="{{ asset('images/cart3.svg') }}" class="align-items-center">
+                                    <img src="{{ asset('images/cart3.svg') }}" class="mb-1">
                                 </a>
                             </div>
                         </div>
