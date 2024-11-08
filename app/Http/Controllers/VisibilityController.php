635f5277c4e7486367a27f52e9e364215f9df84f
Vicente Bercasio <vicentetobio00@gmail.com>
2024-11-08 22:45:17 +0800

    add visibility table, controllers, models, and seeders.

---
+++ app/Http/Controllers/VisibilityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Visibility;
+use Illuminate\Http\Request;
+
+class VisibilityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Visibility  $visibility
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Visibility $visibility)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Visibility  $visibility
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Visibility $visibility)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Visibility  $visibility
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Visibility $visibility)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Visibility  $visibility
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Visibility $visibility)
+    {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(StatusesTableSeeder::class);
+        $this->call(VisibilitiesTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(CoursesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
