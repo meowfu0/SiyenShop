33e39c203fa5e32ccdcb00c8a4fd496bb1333071
Jervy Jake Morales <jeikudevs@gmail.com>
2024-11-08 21:25:58 +0800

    Fixed some codes in PaymentPage

--- resources/views/user/paymentPage.blade.php
+++ resources/views/user/paymentPage.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <script src="{{ asset('js/cart.js') }}"></script>
 
-    <div class="container pt-5">
+    <div class="container pt-4  ">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-11 w-75">
                 <div class="d-flex justify-content-start align-items-center mb-2">
@@ -13,15 +13,15 @@
                 
                 <!------- Sample data ------->                
                 <div class="container mt-2">
-                    <div class="card border border-primary p-4">
+                    <div class="card border border-primary p-3">
                         <div class="card-body">
                             <form>
-                                <div class="mb-3">
+                                <div class="mb-2">
                                     <label for="paymentMode" class="form-label fs-4">
                                         <span class="fs-5 fw-bold text-primary">Mode Of Payment: </span> GCash
                                     </label>
                                 </div>
-                                <p>Please ensure that you make a payment of <span class="fw-bolder text-primary">₱ 600.00</span> to <span class="fw-bolder text-primary">CirCUITS</span>, and remember to provide accurate information.</p>
+                                <p class="fs-4">Please ensure that you make a payment of <span class="fw-bolder text-primary fs-3">₱ 600.00</span> to <span class="fw-bolder text-primary">CirCUITS</span>, and remember to provide accurate information.</p>
 
                                 <div class="mb-3">
                                     <label for="gcashNumber" class="form-label fw-bold">Pay to this GCash Number:</label>
@@ -34,7 +34,7 @@
 
                                 <div class="mb-3">
                                     <label for="referenceNumber" class="form-label fw-bold">Reference Number</label>
-                                    <input type="text" class="form-control" id="referenceNumber" placeholder="Enter reference number" required>
+                                    <input type="text" class="form-control fs-3" id="referenceNumber" placeholder="Enter reference number" required>
                                 </div>
 
                                 <div class="mb-3">
@@ -43,8 +43,8 @@
                                 </div>
 
                                 <div class="d-flex justify-content-end">
-                                    <a href="{{ route('checkOutPage') }}" class="btn btn-outline-primary me-1">Cancel</a>
-                                    <a href="#" data-bs-toggle="modal" data-bs-target="#ModalProceedPayment" class="btn btn-primary">
+                                    <a href="{{ route('checkOutPage') }}" class="btn btn-outline-primary me-1 payment-sizes">Cancel</a>
+                                    <a href="#" data-bs-toggle="modal" data-bs-target="#ModalProceedPayment" class="btn btn-primary payment-sizes">
                                         Confirm Payment
                                         <img src="{{ asset('images/credit-card.svg') }}" class="mb-1">
                                     </a>
