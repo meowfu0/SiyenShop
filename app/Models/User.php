6bbdf40dca3555e03bdfbf373464230ccc49e729
itsyxngshin <acabalbag22@gmail.com>
2024-11-11 00:46:43 +0800

    Updated User relationships with course, status and role

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Course;
+use App\Models\Status;
 
 class User extends Authenticatable
 {
@@ -19,6 +21,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'first_name', 'last_name', 'phone_number', 'course_bloc', 'course_id', 'year', 'email', 'password',
+        'role_id', 'status_id'
     ];
 
     /**
@@ -56,14 +59,13 @@ class User extends Authenticatable
      */
     public function role()
     {
-        return $this->belongsTo(Role::class);
+        return $this->belongsTo(Role::class, 'role_id');
     }
 
     public function status()
 {
-    return $this->belongsTo(Status::class);
+    return $this->belongsTo(Status::class, 'status_id');
 }
-
     
 }
 
