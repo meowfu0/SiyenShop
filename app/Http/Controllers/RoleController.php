a64c2190d203f7d7d5314f1271945b2eb66b7b1b
itsyxngshin <acabalbag22@gmail.com>
2024-11-11 00:53:00 +0800

    Removed limit (retrieved version)

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -14,8 +14,7 @@ class RoleController extends Controller
      */
     public function index()
     {
-        $users = User::all(); 
-        return view('users.index', compact('users'));
+        
     }
 
     /**
--- app/Http/Middleware/RoleAccessValidate.php
+++ app/Http/Middleware/RoleAccessValidate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RoleAccessValidate
 {
@@ -11,11 +12,20 @@ class RoleAccessValidate
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param  \Closure  $next
+     * @param  mixed  ...$roles  (roles passed as parameters)
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next, ...$roles)
     {
+        // Check if the user is authenticated and has a role in the provided list of roles
+        $user = Auth::user();
+        
+        if (!$user || !in_array($user->role->name, $roles)) {
+            // Redirect to a specific page or abort with a 403 Forbidden status
+            return abort(403, 'Unauthorized access');
+        }
+
         return $next($request);
     }
 }
