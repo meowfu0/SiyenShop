2ea2af3689cfb7ee3323fbb46318176c25f5af11
itsyxngshin <acabalbag22@gmail.com>
2024-11-11 08:36:31 +0800

    Minor fixed to shop, status, and user models

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Course;
+use App\Models\Status;
+use App\Models\User;
 
 class Shop extends Model
 {
     use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'shop_name', 'shop_description', 'shop_logo', 'user_id', 'course_id', 'status_id'
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'course_id'); 
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'status_id');
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Role;
 use App\Models\Course;
 use App\Models\Status;
 
