ceb6043cccde009e0cce38ebeea7ccc28c5e188b
John Dave Bañas <dave@Johns-MacBook-Air.local>
2024-11-29 22:15:45 +0800

    Fix case sensitivity issue with AdminFaqs.php

--- app/Http/Livewire/admin/AdminFaqs.php
+++ app/Http/Livewire/admin/AdminFaqs.php
@@ -9,10 +9,7 @@ class AdminFaqs extends Component
 {
     public function render()
     {
-        $faqs = Faq::whereIn('status_id', [1, 3])->get(); 
-
-        return view('livewire.admin.admin-faqs', ['faqs' => $faqs]);
+        $faqs = Faq::all();
+        return view('livewire.admin.admin-faqs', compact('faqs'));
     }
-    
 }
-
