f0ec30dde39c188a9441577cd21a02bc33f137df
itsyxngshin <acabalbag22@gmail.com>
2024-12-04 13:31:39 +0800

    Restored hidden input element for fetching of userId

--- resources/views/livewire/admin/admin-users.blade.php
+++ resources/views/livewire/admin/admin-users.blade.php
@@ -88,6 +88,7 @@
 
                     <!-- User Info Section -->
                     <div class="user-info-container">
+                        <input id="userId" hidden>
                         <p class="user-name"><span id="modalName"></span></p>
                         <p><span id="modalStatus"></span></p>
                         <table class="user-info-table">
